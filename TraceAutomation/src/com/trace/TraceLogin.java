15271a7693de74fa7aaa591d88387c3374c9611a
Maruthi <maruthirajabn@gmail.com>
2022-02-19 20:30:08 +0530

    First Commit.....

---
+++ TraceAutomation/src/com/trace/TraceLogin.java
@@ -0,0 +1,16 @@
+package com.trace;
+
+public class TraceLogin {
+
+	public static void main(String[] args)
+	{
+		browserlaunch();
+
+	}
+	
+	static void browserlaunch()
+	{
+		System.out.println("Browser launched");
+	}
+
+}
